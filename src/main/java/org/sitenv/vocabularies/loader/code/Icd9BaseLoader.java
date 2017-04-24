1d7b1d1dee9fb8d0f6b3f23501962d8e0bf1f2b6
Lakshmi Dintakurty <lakshmi.dintakurty@cerner.com>
2017-04-24 15:08:29 -0400

    Initial Cerner's performance changes

--- src/main/java/org/sitenv/vocabularies/loader/code/Icd9BaseLoader.java
+++ src/main/java/org/sitenv/vocabularies/loader/code/Icd9BaseLoader.java
@@ -1,70 +1,72 @@
 package org.sitenv.vocabularies.loader.code;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.StrBuilder;
 import org.apache.log4j.Logger;
 import org.sitenv.vocabularies.loader.BaseCodeLoader;
-import org.sitenv.vocabularies.loader.VocabularyLoader;
+import org.sitenv.vocabularies.validation.dao.CodeSystemCodeDAO;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
+import org.sitenv.vocabularies.validation.dao.CodeSystemCodeDAO;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
+import static org.sitenv.vocabularies.loader.code.IcdLoader.buildDelimitedIcdCode;
+
 /**
  * Created by Brian on 2/7/2016.
  */
-@Component(value = "SNOMED-CT")
-public class SnomedLoader extends BaseCodeLoader implements VocabularyLoader {
-    private static Logger logger = Logger.getLogger(SnomedLoader.class);
+public abstract class Icd9BaseLoader extends BaseCodeLoader {
+    private static Logger logger = Logger.getLogger(Icd9BaseLoader.class);
+    protected String oid;
 
+    @Override
     public long load(List<File> filesToLoad, DataSource datasource) {
         long n = 0;
-        JdbcTemplate t = new JdbcTemplate(datasource);
         BufferedReader br = null;
         FileReader fileReader = null;
+        JdbcTemplate t = new JdbcTemplate(datasource);
         try {
             StrBuilder insertQueryBuilder = new StrBuilder(codeTableInsertSQLPrefix);
             int totalCount = 0, pendingCount = 0;
 
             for (File file : filesToLoad) {
                 if (file.isFile() && !file.isHidden()) {
-                    logger.debug("Loading SNOMED File: " + file.getName());
+                    logger.debug("Loading ICD9 File: " + file.getName());
                     String codeSystem = file.getParentFile().getName();
-                    int count = 0;
                     fileReader = new FileReader(file);
                     br = new BufferedReader(fileReader);
-                    String available;
-                    while ((available = br.readLine()) != null) {
-                        if ((count++ == 0)) {
-                            continue; // skip header row
-                        } else {
-                            String[] line = StringUtils.splitPreserveAllTokens(available, "\t", 9);
-                            String code = line[4];
-                            String displayName = line[7];
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        if (!line.isEmpty()) {
+                            String code = buildDelimitedIcdCode(line.substring(0, 5));
+                            String displayName = line.substring(6);
+//                            buildCodeInsertQueryString(insertQueryBuilder, code, displayName, codeSystem, oid);
 //                            if (pendingCount++ > 0) {
 //                                insertQueryBuilder.append(",");
 //                            }
 //                            insertQueryBuilder.append("(");
 //                            insertQueryBuilder.append("DEFAULT");
-//                            insertQueryBuilder.append(",'");
-//                            insertQueryBuilder.append(line[4].toUpperCase());
+//                            insertQueryBuilder.append("'");
+//                            insertQueryBuilder.append(buildDelimitedIcdCode(line.substring(0, 5).trim()).toUpperCase());
 //                            insertQueryBuilder.append("','");
-//                            insertQueryBuilder.append(line[7].toUpperCase().replaceAll("'", "''"));
+//                            insertQueryBuilder.append(line.substring(6).trim().toUpperCase().replaceAll("'", "''"));
 //                            insertQueryBuilder.append("','");
 //                            insertQueryBuilder.append(file.getParentFile().getName());
 //                            insertQueryBuilder.append("','");
-//                            insertQueryBuilder.append(CodeSystemOIDs.SNOMEDCT.codesystemOID());
+//                            insertQueryBuilder.append(oid);
 //                            insertQueryBuilder.append("')");
 
                             n++;
-                            t.update(codeTableInsertSQLPrefix,code.toUpperCase(),displayName.toUpperCase(),codeSystem,CodeSystemOIDs.SNOMEDCT.codesystemOID());
-//                            t.update(insertQueryPrefix,line[4].toUpperCase(),line[7].toUpperCase(),file.getParentFile().getName(),CodeSystemOIDs.SNOMEDCT.codesystemOID());
+                            t.update(codeTableInsertSQLPrefix,code.trim().toUpperCase(),displayName.trim().toUpperCase(),codeSystem,oid);
+
 //                            if ((++totalCount % 5000) == 0) {
 //                                doInsert(insertQueryBuilder.toString(), connection);
 //                                insertQueryBuilder.clear();
